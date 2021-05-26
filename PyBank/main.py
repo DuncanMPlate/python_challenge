6d7be4061834edff2a343092e81d23e549298f8f
Duncan Plate <duncanplate@gmail.com>
2021-05-25 23:57:41 -0500

    Day 1 commit Failed to properly loop for sum of dates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,15 @@
-hot_fudge_sunday == False
+import csv
+import os
+import math
+
+output_path = os.path.join(os.getcwd(), 'Resources', 'budget_data.csv')
+
+
+#open file in write mode
+with open(output_path, 'r', newline='') as csvfile:
+    #initialise writer
+    csvreader = csv.reader(csvfile, delimiter=',')
+    for r in csvreader:
+        print(csvfile)
+
+
